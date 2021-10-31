4ac344c13e8dd9c5d7bca14429d971d9bf02b821
Kondaveeti <50198660+Baldur008@users.noreply.github.com>
2021-10-31 14:24:26 +0530

    Added logic

--- lib/main.dart
+++ lib/main.dart
@@ -381,18 +381,15 @@ class _AnimatingPercentageShowerState extends State<AnimatingPercentageShower> {
 
   @override
   Widget build(BuildContext context) {
+    
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: Column(
         verticalDirection: VerticalDirection.up,
         children: [
-          [1, 2, 3, 4],
-          [5, 6, 7, 8],
-          [9, 10, 11, 12],
-          [13, 14, 15, 16],
-          [17, 18, 19, 20],
-          [21, 22, 23, 24],
-          [25, 26, 27, 28]
+          for(int l=0,i=1;l<7;++l) [
+            for(int j=0; j<4;++j,++i) i
+          ]         
         ]
             .map<Widget>((e) => Expanded(
                   child: Row(
